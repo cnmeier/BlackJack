a59e0ef05e1d637ece8a1291bac60cefba86f7bc
rmmartino <rmmartino@loyola.edu>
2019-09-22 20:34:32 -0400

    game class creation

---
+++ Game.java
@@ -0,0 +1,19 @@
+package BlackJack;
+
+public class Game
+{
+    public static void main(String[] args)
+    {
+
+
+    /* purpose:
+     * input:
+     * returns:
+     */
+
+     public String test()
+     {
+        return "I am right arm";
+     }
+     }
+}
