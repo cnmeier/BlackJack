e5bd3ce639a7ddd420c6282db420398583849947
rmmartino <rmmartino@loyola.edu>
2019-09-22 23:55:35 -0400

    add new card

--- Card.java
+++ Card.java
@@ -3,15 +3,15 @@
 
 public class Card
 {
-   int suit = 0;
-   int rank = 0;
+   char suit = '\0';
+   char rank = '\0';
 
    /* 
     * Purpose: To set the suit and rank of a card
     * Input: The suit and rank
     * Return: Nothing
     */
-    public Card(int suit, int rank)
+    public Card(char suit, char rank)
     {
         this.suit = suit;
         this.rank = rank;
--- Hand.java
+++ Hand.java
@@ -5,28 +5,54 @@ import java.util.*;
 
 public class Hand
 {
-    List hand = new ArrayList(); 
-    Card card = new Card();
+    List<Card> hand = new ArrayList<>(); 
+    private int points = 0;
+    //Card card = new Card(void, void);
  
     public Hand()
     {
     }
 
-    /* 
-     * Purpose: To create a Hand object with the first two cards
-     * @param: The first card dealt
-     * @param: The second card dealt
-     * @return: Nothing
-     */
-     public create_hand(Card card1, Card card2)
-     {
-         hand.add(card1);
-         hand.add(card2);       
-     }
+   /* 
+    * Purpose: To create a Hand object with the first two cards
+    * @param: The first card dealt
+    * @param: The second card dealt
+    * @return: Nothing
+    */
+    public void createHand(Card card1, Card card2)
+    {
+        hand.add(card1);
+        hand.add(card2);       
+    }
 
+   /* 
+    * Purpose: Add a card to the hand
+    * @param: The new card
+    * @return: Nothing
+    */
+    public void addCard(Card newCard)
+    {
+        hand.add(newCard);
+    }
     
-    //add a card to the list
-    //calculate the current count
-
+   /* 
+    * Purpose: Update the current number of points in the hand
+    * @return: The current number of points
+    */
+    public void updatePoints()
+    {
+        for(int i = 0; i < hand.size(); i++)
+        {
+            if ('2' <= hand.get(i).rank && (char)10 >= hand.get(i).rank)
+                points += Integer.valueOf(hand.get(i).rank);
+            else if (hand.get(i).rank == 'J' || hand.get(i).rank == 'Q' || hand.get(i).rank == 'K')
+                points += 10;
+            else 
+                if (points <= 10)
+                    points += 11;
+                else
+                    points += 1;  
+        }
+    }
 
 }
