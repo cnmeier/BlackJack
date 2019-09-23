faa0ffa94ec12830c57ddff6e7f4a4e277de6853
rmmartino <rmmartino@loyola.edu>
2019-09-22 21:49:06 -0400

    card class

---
+++ Card.java
@@ -0,0 +1,20 @@
+
+
+public class Card
+{
+   int suit = 0;
+   int rank = 0;
+
+   /* 
+    * Purpose: To set the suit and rank of a card
+    * Input: The suit and rank
+    * Return: Nothing
+    */
+    public Card(int suit, int rank)
+    {
+        this.suit = suit;
+        this.rank = rank;
+    }
+}
+
+
