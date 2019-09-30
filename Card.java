8ba2647c9c64e0feda5ef52d917ddc520593f818
Chelsea Meier <31703745+cnmeier@users.noreply.github.com>
2019-09-30 18:27:19 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,20 +1,29 @@
-// Rebecca Martino and Chelsea Meier
-// September 22, 2019
+/**
+ * @author Rebecca Martino and Chelsea Meier
+ * September 30th, 2019
+ * CS482
+ */
 
-public class Card
-{
-   char suit = '\0';
-   char rank = '\0';
+package com.example.chelseameier.blackjack;
 
-   /* 
-    * Purpose: To set the suit and rank of a card
-    * @param: suit, the suit of the card (hearts, diamonds, spades, or clubs)
-    * @param: rank, the rank of the card (1-10, Jack, Queen, King, or Ace)
-    * @return: Nothing
-    */
-    public Card(char suit, char rank)
+public class Card {
+
+    String suit = "";
+    String rank = "";
+    String filename;
+
+    /**
+     * Purpose: To set the suit and rank of a card
+     *
+     * @param: suit
+     *            the suit of the card (hearts, diamonds, spades, or clubs)
+     * @param: rank
+     *            the rank of the card (1-10, Jack, Queen, King, or Ace)
+     */
+    public Card(String suit, String rank)
     {
         this.suit = suit;
         this.rank = rank;
+        this.filename= rank + "_of_" + suit + ".png";
     }
 }
